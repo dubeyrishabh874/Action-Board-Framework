2d9dcc51e440e97f47950376a0852c35b5e637ef
Rishabh Dubey <dubeyrishabh874@gmail.com>
2025-04-29 15:18:07 +0530

    Video Player UI Scrren changes

--- src/containers/ActionBoard/index.tsx
+++ src/containers/ActionBoard/index.tsx
@@ -1,9 +1,13 @@
 "use client";
+import styles from "../../page.module.css";
+import VideoMediaPlayer from "../VideoMediaPlayer";
 
-import React from "react";
-
-const ActionBoard: React.FC = () => {
-  return <>welcome</>;
-};
-
-export default ActionBoard;
+export default function ActionBoard() {
+  return (
+    <div className={styles.page} onContextMenu={(e) => e.preventDefault()}>
+      <main className={styles.main}>
+        <VideoMediaPlayer />
+      </main>
+    </div>
+  );
+}
